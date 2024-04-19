af1c3ebadfd9f8aca334da201f1ad61f6d33c694
David <davka8989@gmail.com>
2024-04-19 22:05:39 +0300

    pass stage

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,6 @@ use std::{
     thread,
 };
 
-// const CRLF: &str = "";
-// const RESPONSE_200: &str = "HTTP/1.1 200 OK\r\n\r\n";
-// const RESPONSE_404: &str = "HTTP/1.1 404 Not Found\r\n\r\n";
-
-
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
@@ -35,19 +30,45 @@ fn main() {
 }
 
 fn build_response(request: String) -> String {
+    println!("reqeust: {}", request);
     let lines: Vec<&str> = request.split("\r\n").collect();
     if lines.is_empty() {
         return "HTTP/1.1 400 BAD REQUEST\r\n\r\n".to_string();
     }
 
     let req_target: Vec<&str> = lines[0].split_whitespace().collect();
+    let method = req_target[0];
     let path = req_target[1];
     let _req_host: Vec<&str> = lines[1].split_whitespace().collect();
     let req_user_agent: Vec<&str> = lines[2].split_whitespace().collect();
 
+    if method == "POST" && path.starts_with("/files/") {
+        let filename = &path[7..];
+
+        let args = env::args().collect::<Vec<String>>();
+        let dir = args[2].clone();
+        let filename = format!("{}/{}", dir, filename);
+        println!("Filename: {}", filename);
+
+        let request_body_start = request.find("\r\n\r\n").map(|i| i + 4).unwrap_or(request.len());
+        let request_body = &request.as_bytes()[request_body_start..];
+        let request_body_without_nulls = String::from_utf8_lossy(request_body)
+            .replace("\x00", "")
+            .into_bytes();
+
+        match fs::write(&filename, request_body_without_nulls) {
+            Ok(_) => return "HTTP/1.1 201 Created\r\n\r\n".to_string(),
+            Err(e) => {
+                eprintln!("Error writing file: {}", e);
+                return "HTTP/1.1 500 Internal Server Error\r\n\r\n".to_string();
+            }
+        }
+    }
+
     if path == "/" {
         return "HTTP/1.1 200 OK\r\n\r\n".to_string();
     }
+
     if path == "/user-agent" {
         let user_agent = req_user_agent[1];
         return format!(
@@ -55,6 +76,7 @@ fn build_response(request: String) -> String {
             user_agent.len(),
             user_agent
         );
+
     } else if path.starts_with("/echo/") {
         let random_string = path.trim_start_matches("/echo/");
         return format!(
@@ -62,6 +84,7 @@ fn build_response(request: String) -> String {
             random_string.len(),
             random_string
         );
+
     } else if path.starts_with("/files") {
         let filename = &path[7..];
         let args = env::args().collect::<Vec<String>>();
